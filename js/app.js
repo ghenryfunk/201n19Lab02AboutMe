ac99341ec2d6125da2c4ed4017fc3db7484ebf32
Henry Funk <georgehenryfunk@gmail.com>
2020-07-29 09:18:05 -0700

    question 6 and 7 clean up

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('Hello World!');
+// console.log('Hello World!');
 
 var userName = prompt('Hello! What is your name?');
 
@@ -97,67 +97,126 @@ if (question5 === 'yes' || question5 === 'y') {
   alert('You\'re right. I have NO teeth left... from all the sugar.');
 }
 
-///////////////////////////////////QUESTION 6 IS KILLING ME!!!!!/////////
-///////////I CANNOT FIGURE OUT HOW TO MAKE THIS COUNTER WORK!!! IT MAKES SENSE TO ME WRITTEN AS IS and the counter actually does seem to work... but the for loop with the if statements won't work. I couldn't figure this question out last time I took 201 and I still cannot figure it out. Please help me! I need some serious one-one time with this to trace the path the code is taking as it reads this code. I need a visual map of it going from one statement to the next because this refuses to make sense to me!!!!!/////////////////////////////////////////////////////////////////
-for (var i = 4; i >= 0; i--) {
-  var question6 = prompt(`What do you think my favorite number is, ${userName}? You have ${i} guesses.`);
-  console.log(i);
-  console.log(question6);
-  while (isNaN(question6)) {
-    i--;
-    question6 = prompt(`Please answer with a number, ${userName}. You have ${i} guesses left.`);
-    console.log(i);
-    console.log(question6);
-  }
-  if (question6 > 33) {
-    i--;
-    question6 = prompt(`Too high, ${userName}. You have ${i} guesses left.`);
-    console.log(i);
-    console.log(question6);
-  }
-  else if (question6 < 33) {
-    i--;
-    question6 = prompt(`Too low, ${userName}. You have ${i} guesses left.`);
-    console.log(i);
-    console.log(question6);
-  }
-  else if (question6 === 33) {
-    question6 = prompt(`You got it, ${userName}! Well done!`);
+// ///////////////////////////////////QUESTION 6 IS KILLING ME!!!!!/////////
+// ///////////I CANNOT FIGURE OUT HOW TO MAKE THIS COUNTER WORK!!! IT MAKES SENSE TO ME WRITTEN AS IS and the counter actually does seem to work... but the for loop with the if statements won't work. I couldn't figure this question out last time I took 201 and I still cannot figure it out. Please help me! I need some serious one-one time with this to trace the path the code is taking as it reads this code. I need a visual map of it going from one statement to the next because this refuses to make sense to me!!!!!/////////////////////////////////////////////////////////////////
+// for (var i = 4; i > 0; i--) {
+//   var question6 = prompt(`What do you think my favorite number is, ${userName}? You have ${i} guesses.`);
+//   console.log(i);
+//   console.log(question6);
+//   if (isNaN(question6)) {
+//     i--;
+//     question6 = prompt(`Please answer with a number, ${userName}. You have ${i} guesses left.`);
+//     console.log(i);
+//     console.log(question6);
+//   }
+//   else if (question6 > 33) {
+//     i--;
+//     question6 = prompt(`Too high, ${userName}. You have ${i} guesses left.`);
+//     console.log(i);
+//     console.log(question6);
+//   }
+//   else if (question6 < 33) {
+//     i--;
+//     question6 = prompt(`Too low, ${userName}. You have ${i} guesses left.`);
+//     console.log(i);
+//     console.log(question6);
+//   }
+//   else if (question6 === 33) {
+//     question6 = prompt(`You got it, ${userName}! Well done!`);
+//     // correctAnswer++;
+//     i === 0;
+//     console.log(i);
+//     console.log(question6);
+//   }
+//   else {
+//     alert(`The correct answer was 33, ${userName}`);
+//     console.log(i);
+//     console.log(question6);
+//   }
+// }
+
+// function question6() {
+//   var guesses = 4;
+//   var question6 = parseInt(prompt(`What do you think my favorite number is? You have ${guesses} guesses left`));
+//   for (var i = 0; i < guesses; i++) {
+//     if(isNaN(question6) && i < guesses) {
+//       i++;
+//       question6 = parseInt(prompt(`Enter a number please. You have ${4-i} guesses left`));
+//       console.log(i);
+//     }
+//     else if (question6 > 33 && i < guesses) {
+//       i++;
+//       question6 = parseInt(prompt(`Too high! You have ${4-i} guesses left`));
+//       console.log(i);
+//     }
+//     else if (question6 < 33 && i < guesses) {
+//       i++;
+//       question6 = parseInt(prompt(`Too low! You have ${4-i} guesses left`));
+//       console.log(i);
+//     }
+//     else if (question6 === 33 && i < guesses) {
+//       alert('You got it! 33 is a great number!');
+//       i === 4;
+//       console.log(i);
+//     }
+//     else {
+//       alert('You are out of guesses :( Correct answer was 33!');
+//       console.log(i);
+//     }
+//   }
+// }
+// question6();
+
+//THIRD TRY AT QUESTION 6!!!!//////////////////////
+var guesses = 4;
+var correctNumber = 33;
+var answeredCorrectly = false;
+
+var question6 = parseInt(prompt(`What is my favorite number? You have ${guesses} guesses.`));
+
+while (guesses > 0 && !answeredCorrectly) {
+  guesses--;
+  if (question6 > correctNumber && guesses > 0) {
+    question6 = parseInt(prompt(`Too high! You have ${guesses} guesses left.`));
+  } else if (question6 < correctNumber && guesses > 0) {
+    question6 = parseInt(prompt(`Too low! You have ${guesses} guesses left.`));
+  } else if (correctNumber === question6) {
+    alert('Yes! You got it!');
+    answeredCorrectly = true;
     correctAnswer++;
-    i = 0;
-    console.log(i);
-    console.log(question6);
+  } else if (isNaN(question6)) {
+    question6 = parseInt(prompt(`Please enter a number. You have ${guesses} guesses left.`));
   }
-  else {
-    alert(`The correct answer was 33, ${userName}`);
-    console.log(i);
-    console.log(question6);
-  }
+}
+if (guesses === 0 && !answeredCorrectly) {
+  alert(`Almost... Correct answer was ${correctNumber}.`);
 }
 
 var movieArray = ['point break', 'point break', 'point break'];
+var guesses7 = 6;
+var answeredCorrectly7 = false;
 
-for (var i = 6; i >= 0; i--) {
-  var question7 = prompt(`What is the greatest movie ever made, ${userName}? You have ${i} guesses.`).toLowerCase();
-  console.log(i);
-  console.log(question7);
-  if (question7 === movieArray[0] || question7 === movieArray[1] || question7 === movieArray[2]){
-    alert(`Duh! Point Break is the greatest film ever made, ${userName}! Other acceptable answers were "Point Break," and "Point Break."`);
-    i = 0;
-    correctAnswer++;
-    console.log(i);
-    console.log(question6);
+var question7 = prompt(`What is the greatest movie ever made, ${userName}? You have ${guesses7} guesses.`).toLowerCase();
+
+while (guesses7 > 0 && !answeredCorrectly7) {
+  guesses7--;
+  console.log(guesses7);
+  for (var i = 0; i < movieArray.length; i++) {
+    if (question7 === movieArray[i]) {
+      alert(`Duh! Point Break is the greatest film ever made, ${userName}! Other acceptable answers were "Point Break," and "Point Break."`);
+      correctAnswer++;
+      answeredCorrectly7 = true;
+      i = movieArray.length;
+    }
   }
-  else {
-    alert(`Afraid not, ${userName}. C'mon, this should be an easy one! You have ${i} guesses left.`);
+  if (guesses7 > 0 && !answeredCorrectly7) {
+    question7 = prompt(`Afraid not, ${userName}. C'mon, this should be an easy one! You have ${guesses7} guesses left.`).toLowerCase();
   }
+}
+if (guesses7 === 0 && !answeredCorrectly7) {
+  alert(`Sorry but none of those was the best movie ever made ${userName}! Acceptable answers were "Point Break," and "Point Break."`);
 }
 
 alert(`Thank you for playing my guessing game, ${userName}! You got ${correctAnswer} answer(s) right. Good job!`);
 
-console.log(correctAnswer);
-
-
-
-
-
+// console.log(correctAnswer);
